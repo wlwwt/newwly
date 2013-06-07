475c8a55eb6edb98b3c52f5e2a187fb5bfb6df9a
Wolfgang Freund <wolfgang.freund@cyberhouse.at>
2013-06-07 22:43:45 +0200

    bootstrap theme for forms

--- src/Newwly/UserBundle/Form/Type/RegistrationFormType.php
+++ src/Newwly/UserBundle/Form/Type/RegistrationFormType.php
@@ -11,10 +11,67 @@ class RegistrationFormType extends BaseType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        parent::buildForm($builder, $options);
-
-        // add your custom field
-        $builder->add('kindlemail', null, array('invalid_message' => 'fos_user.password.mismatch'));
+        $builder
+            ->add('username', 'text', array(
+                'label' => 'form.username',
+                'label_attr' => array(
+                    'class' => 'control-label'
+                ),
+                'translation_domain' => 'FOSUserBundle',
+                'attr' => array(
+                    'class' => 'input-block-level',
+                    'placeholder' => 'form.username',
+                    )
+                ))
+            ->add('email', 'email', array(
+                'label' => 'form.email', 
+                'label_attr' => array(
+                    'class' => 'control-label'
+                ),
+                'translation_domain' => 'FOSUserBundle',
+                'attr' => array(
+                    'class' => 'input-block-level',
+                    'placeholder' => 'form.email',
+                    )
+                ))
+            ->add('plainPassword', 'repeated', array(
+                'type' => 'password',
+                'options' => array(
+                    'translation_domain' => 'FOSUserBundle',
+                ),
+                'first_options' => array(
+                    'label' => 'form.password',
+                    'label_attr' => array(
+                        'class' => 'control-label'
+                    ),
+                    'attr' => array(
+                        'class' => 'input-block-level',
+                        'placeholder' => 'form.password',
+                    )),
+                'second_options' => array(
+                    'label' => 'form.password_confirmation',
+                    'label_attr' => array(
+                        'class' => 'control-label'
+                    ),
+                    'attr' => array(
+                        'class' => 'input-block-level',
+                        'placeholder' => 'form.password_confirmation',
+                    )),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ))
+            ->add('kindlemail', 'text', array(
+                'translation_domain' => 'FOSUserBundle',
+                'label' => 'form.kindlemail',
+                'label_attr' => array(
+                    'class' => 'control-label'
+                ),
+                'invalid_message' => 'fos_user.password.mismatch',
+                'attr' => array(
+                    'class' => 'input-block-level',
+                    'placeholder' => 'form.kindlemail',
+                    )
+                ))
+        ;
     }
 
     public function getName()
